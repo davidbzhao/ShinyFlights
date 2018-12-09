48e4bfdb5c907f9e1a94336b67a4f2df8cb5f340
davidbzhao <dz6hu@virginia.edu>
2018-12-09 17:23:07 -0500

    Introduce fares

--- leaflet.R
+++ leaflet.R
@@ -4,12 +4,29 @@ library(tidyverse)
 library(leaflet)
 library(htmltools)
 library(shinyBS)
+library(data.table)
 
 # ======== Data input
 spark <- spark_connect(master="local")
 all <- spark_read_csv(spark, name="flight", path="all.csv", header=T)
 coords <- read_csv("usairports.csv")
 hubs <- c("PHX","LAX","SAN","SFO","DEN","MIA","FLL","MCO","TPA","ATL","MDW","ORD","IND","CVG","SDF","BWI","BOS","DTW","MSP","MCI","STL","LAW","EWR","TTN","JFK","LGA","CLT","GSO","CLE","LUK","DAY","PDX","PHL","PIT","CAE","MEM","BNA","DFW","DAL","IAH","SLC","IAD","DCA","SEA")
+library(data.table)
+library(tidyverse)
+
+setwd("D:\\Downloads\\faredata")
+fares <- fread('fares.csv')
+airport_codes <- read.csv('AIRPORT_code.csv', header=T, stringsAsFactors = T)
+filtered <- fares[DOLLAR_CRED == 1 & ONLINE == 1 & ROUNDTRIP == 1]
+
+fares_clean <- filtered %>%
+  group_by(YEAR, ORIGIN_AIRPORT_ID, REPORTING_CARRIER) %>%
+  summarize(ITIN_YIELD_AVG = mean(ITIN_YIELD), ITIN_YIELD_COUNT = n()) %>%
+  ungroup() %>%
+  mutate(ORIGIN_AIRPORT_ID = as.factor(ORIGIN_AIRPORT_ID)) %>%
+  inner_join(airport_codes, by=c("ORIGIN_AIRPORT_ID" = "Code.1")) %>%
+  select(YEAR, Code, REPORTING_CARRIER, ITIN_YIELD_AVG, ITIN_YIELD_COUNT) %>%
+  mutate(Code = as.character(Code))
 
 # ======== Filter and transform
 coords <- coords %>%
@@ -23,9 +40,10 @@ clean <- all %>%
 clean_local <- clean %>%
   ungroup() %>%
   collect() %>%
+  mutate(OP_ORIG_CARRIER = OP_UNIQUE_CARRIER) %>%
   mutate(OP_UNIQUE_CARRIER = recode(OP_UNIQUE_CARRIER,"CO"="AA","EV"="OO","FL"="WN","HP"="AA","MQ"="AA","US"="AA","NW"="DL","XE"="OO","TZ"="WN","OH"="AA","9E"="DL","VX"="AS")) %>%
   group_by(YEAR, ORIGIN, OP_UNIQUE_CARRIER, ORIGIN_CITY_NAME) %>%
-  summarize(count=sum(count)) %>%
+  summarize(count=sum(count), OP_ORIG_CARRIER=max(OP_ORIG_CARRIER)) %>%
   ungroup() %>%
   group_by(YEAR, ORIGIN) %>%
   top_n(6, count) %>%
@@ -36,6 +54,13 @@ clean_local <- clean %>%
 # Fix Longitude
 clean_local <- clean_local %>%
   mutate(Longitude = -1 * abs(Longitude))
+
+# Add fares
+clean_local <- clean_local %>%
+  inner_join(fares_clean, by=c("ORIGIN" = "Code",
+                               "YEAR" = "YEAR",
+                               "OP_UNIQUE_CARRIER" = "REPORTING_CARRIER"))
+
 
 # Normalize Count
 max_size <- 25
@@ -73,14 +98,13 @@ ui <- fluidPage(
     "
   ),
   titlePanel(
-    h1("Domestic Flight Dominance at Hubs (2003-2017)",
+    h2("Domestic Flight Dominance at Hubs (2003-2017)",
        style="text-align:center")
   ),
   div(
     leafletOutput("mymap"),
     style="padding:4rem"
   ),
-  br(),
   div(
       id="sliderContainer",
       sliderInput(inputId="year",
@@ -90,10 +114,9 @@ ui <- fluidPage(
                   max=2017,
                   sep="",
                   width="100%",
-                  animate=T),
-      animationOptions(interval = 1000, loop = T, playButton = NULL,
-                       pauseButton = NULL)
-  )
+                  animate=T)
+  ),
+  p("Black border = the dominating airline of that year eventually merged into the specified airline")
 )
 
 server <- function(input, output, session) {
@@ -103,8 +126,8 @@ server <- function(input, output, session) {
       filter(isMax) %>%
       ungroup()
   }, ignoreNULL = FALSE)
-  createLabel <- function(city_name) {
-    city_name
+  createLabel <- function(city_name, itin_yield_avg) {
+    paste(city_name, " ($", sprintf("%.2f", itin_yield_avg), ")", sep="")
   }
   output$mymap <- renderLeaflet({
     leaflet(clean_local) %>%
@@ -121,11 +144,13 @@ server <- function(input, output, session) {
                        lng=~Longitude,
                        lat=~Latitude,
                        radius=~size,
-                       color=~palette(name),
-                       label=~createLabel(ORIGIN_CITY_NAME),
+                       fillColor=~palette(name),
+                       color="black",
+                       label=~createLabel(ORIGIN_CITY_NAME, ITIN_YIELD_AVG),
                        labelOptions=labelOptions(opacity=0.9),
-                       opacity=0.9,
-                       fillOpacity=0.5)
+                       fillOpacity=0.5,
+                       opacity=~ifelse(OP_ORIG_CARRIER == OP_UNIQUE_CARRIER, 0, 1),
+                       weight=2)
   })
   
   observeEvent(input$mymap_marker_click, {
@@ -138,18 +163,21 @@ server <- function(input, output, session) {
                mutate(perc = count/sum(count), 0), 
              aes(reorder(name, -perc), perc)) + 
         geom_bar(aes(fill=name), stat="identity") +
+        geom_label(aes(label=paste("$", sprintf("%.2f", ITIN_YIELD_AVG)), sep="")) +
         theme_bw() +
         labs(x="Airline Code", y="Outbound Flight Percentage") +
         scale_y_continuous(labels = scales::percent) +
         scale_fill_manual(name="name", values=airline.colors)
     })
     # print(p$id)
-    showModal(modalDialog(
-      title = paste(input$year, " Airline Dominance in ", p$id),
-      plotOutput("airport_plot"),
-      easyClose=T,
-      size="l"
-    ))
+    showModal(
+      modalDialog(
+        title = paste(input$year, " Airline Dominance in ", p$id),
+        plotOutput("airport_plot"),
+        easyClose=T,
+        size="l"
+      )
+    )
   })
 }
 
